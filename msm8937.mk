056cb6b17db8627116f5d7e7836538c62d1e356a
GiaSen <giammasena@gmail.com>
2020-09-11 15:35:02 +0200

    santoni: build libhwbinder

--- msm8937.mk
+++ msm8937.mk
@@ -130,6 +130,11 @@ PRODUCT_COPY_FILES += \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# Binder
+PRODUCT_PACKAGES += \
+    libhwbinder \
+    libhwbinder.vendor
+
 # Camera
 PRODUCT_PACKAGES += \
     camera.device@1.0-impl \
