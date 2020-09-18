64b44acedd4ae488598c0a501513adc26e036981
sweetyicecare <gabrielseedev@outlook.com>
2020-09-18 02:25:53 +0000

    santoni : Update manifest entries for radio
    Signed-off-by: Dhruv <dhruvgera61@gmail.com>
    
    msm8937-common: Nuke telephony-ext

--- msm8937.mk
+++ msm8937.mk
@@ -350,13 +350,11 @@ PRODUCT_PACKAGES += \
 # Telephony
 PRODUCT_PACKAGES += \
     ims-ext-common_system \
-    telephony-ext \
     ims-ext-common \
     ims_ext_common.xml
 
 PRODUCT_BOOT_JARS += \
     ims-ext-common_system \
-    telephony-ext
 
 # TextClassifier
 PRODUCT_PACKAGES += \
